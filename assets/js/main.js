e88d34d4bfb8c40099f932ddec39d04e3cd819f1
Ji99 <rksk0531@naver.com>
2022-03-20 01:39:26 +0900

    2022.03.20. / 01:39

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,28 @@ let swiper = new Swiper(".myGameList__container", {
     clickable: true,
   },
 });
+
+// ====== Game Board  ======
+
+const gameBoardBtnContainer = document.querySelector(
+  ".gameBoardBtn__container"
+);
+const gameBoardContainer = document.querySelector(".gameBoard__container");
+const gameBoardBtnPlus = document.querySelector(".ph-plus-bold");
+const gameBoardBtnArrow = document.querySelector(".ph-arrow-up-bold");
+
+function expandBoard() {
+  if (gameBoardBtnArrow.classList[1] === "hidden") {
+    gameBoardBtnPlus.classList.add("hidden");
+    gameBoardBtnArrow.classList.remove("hidden");
+    gameBoardContainer.style.height = "775px";
+  } else if (gameBoardBtnPlus.classList[1] === "hidden") {
+    gameBoardBtnArrow.classList.add("hidden");
+    gameBoardBtnPlus.classList.remove("hidden");
+    gameBoardContainer.style.height = "390px";
+  }
+}
+
+function expandBoardBack() {}
+
+gameBoardBtnContainer.addEventListener("click", expandBoard);
